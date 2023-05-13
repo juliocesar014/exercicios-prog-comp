964d4f3801d3c2f719096eb3d51080030e0796a1
juliocesar014 <juliocesar014@live.com>
2023-05-13 20:14:35 -0300

    feat: second exerc

---
+++ exerc2.py
@@ -0,0 +1,12 @@
+"""
+Faça um programa que verifica a validade de uma senha
+fornecida pelo usuário. Se o usuário digitar a senha
+‘123456’, escrever a mensagem ‘Acesso liberado’. Caso
+contrário, escrever ‘Acesso negado’; 
+"""
+
+senha = input("Digite a senha: ")
+if senha == "123456":
+    print("Acesso liberado!")
+else:
+    print("Acesso negado!")
